df05d6aa96370feedd9684f1f931f4ee5b7e3e6b
karykt <42255225+karykt@users.noreply.github.com>
2018-11-14 16:50:55 -0500

    Add files via upload

---
+++ Retrofitapp/src/main/java/com/mtlcollege/retrofittutorial/ApiInterface.java
@@ -0,0 +1,20 @@
+package com.mtlcollege.retrofittutorial;
+
+import retrofit.Callback;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
+public interface ApiInterface {
+
+    @FormUrlEncoded // annotation used in POST type requests
+    @POST("/retrofit/register.php")     // API's endpoints
+    public void registration(@Field("name") String name,
+                             @Field("email") String email,
+                             @Field("password") String password,
+                             @Field("logintype") String logintype,
+                             Callback<SignUpResponse> callback);
+
+// In registration method @Field used to set the keys and String data type is representing a string
+// type value and callback is used to get the response from api and it will set it in our POJO class
+}
